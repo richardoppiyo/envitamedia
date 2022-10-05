f51c8662ed389fa1f5f6c4cdfb7d3051cf9a01dc
Richard Opiyo <richardoppiyo@gmail.com>
2022-10-05 09:19:26 +0300

    solce issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Create from './components/Create/Create';
+// import Create from './components/Create/Create';
 import Landing from './components/Landing/Landing';
 
 const App = () => (
@@ -8,6 +9,7 @@ const App = () => (
     <Routes>
       <Route index element={<Landing />} />
       <Route path="create" element={<Create />} />
+      {/* <Route path="create" element={<Create />} /> */}
     </Routes>
   </Router>
 );
